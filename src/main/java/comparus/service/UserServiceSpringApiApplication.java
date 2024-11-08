dda0fc60df8c80e3768d92a21e3dcd940df3f478
Maksim Kuzmenyuk <mkn_ua@ukr.net>
2024-11-08 10:30:58 +0200

    Create README.md

--- src/main/java/comparus/service/UserServiceSpringApiApplication.java
+++ src/main/java/comparus/service/UserServiceSpringApiApplication.java
@@ -1,12 +1,12 @@
 package comparus.service;
 
-import comparus.service.config.ReadConfig;
+//import comparus.service.config.ReadConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
-@EnableConfigurationProperties(ReadConfig.class)
+//@EnableConfigurationProperties(ReadConfig.class)
 public class UserServiceSpringApiApplication {
 
     public static void main(String[] args) {
