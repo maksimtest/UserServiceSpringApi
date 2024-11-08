c21ce702315e6958bea8f19b57b90ba77a4bc2cb
Your Name <you@example.com>
2024-11-08 11:01:03 +0200

    Build structure according to Open Api specification: 1) Controller/UserController 2) Domain/User

---
+++ src/main/java/comparus/domain/User.java
@@ -0,0 +1,22 @@
+package comparus.service.domain;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.*;
+
+import javax.annotation.processing.Generated;
+
+@Entity
+@Table(name="users")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+   @Id
+    private String id;
+    private String username;
+    private String name;
+    private String surname;
+}
