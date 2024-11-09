32765ef9325950bcc9ca91f96dced884222a0eb6
Your Name <you@example.com>
2024-11-09 18:40:12 +0200

    Implementation ValidateRequestParameters

--- src/main/java/comparus/service/service/ValidateRequestParameters.java
+++ src/main/java/comparus/service/service/ValidateRequestParameters.java
@@ -1,26 +1,67 @@
 package comparus.service.service;
 
+import comparus.service.domain.User;
 import comparus.service.exception.FilterInvalidParametersException;
-import comparus.service.exception.InvalidParametersException;
 import comparus.service.exception.OrderInvalidParametersException;
 import comparus.service.exception.PropagationInvalidParametersException;
 import org.springframework.stereotype.Service;
 
+import java.lang.reflect.Field;
+import java.util.Arrays;
+import java.util.List;
+
 @Service
 public class ValidateRequestParameters {
     public void checkFilterField(String filter) {
-        if(filter.contains("0")){
+        if (filter != null && !filter.isEmpty()) {
+            for (String fName : getFieldsName()) {
+                String begin = fName + "-";
+                if (filter.startsWith(begin) && filter.length() > begin.length()) {
+                    return;
+                }
+            }
             throw new FilterInvalidParametersException(filter);
         }
     }
+
     public void checkPropagationField(String propagation) {
-        if(propagation.contains("0")){
+        if (propagation != null && !propagation.isEmpty()) {
+            int separatorIndex = propagation.indexOf('-');
+            if (separatorIndex > 0 && separatorIndex < propagation.length() - 1) {
+                String num1 = propagation.substring(0, separatorIndex);
+                String num2 = propagation.substring(separatorIndex+1);
+                if(isIntegerMoreZero(num1) && isIntegerMoreZero(num2)) {
+                    return;
+                }
+            }
             throw new PropagationInvalidParametersException(propagation);
         }
     }
-    public void checkOrderField(String order){
-        if(order.contains("0")){
+    public boolean isIntegerMoreZero(String value){
+        try{
+            if(Integer.parseInt(value) >= 0){
+                return true;
+            }
+        } catch (Exception e){
+
+        }
+        return false;
+    }
+
+    public void checkOrderField(String order) {
+        if (order != null && !order.isEmpty()) {
+            for (String fname : getFieldsName()) {
+                if (order.equals(fname) || order.equals("-" + fname)) {
+                    return;
+                }
+            }
             throw new OrderInvalidParametersException(order);
         }
+    }
+
+    public List<String> getFieldsName() {
+        return Arrays.stream(User.class.getDeclaredFields())
+                .map(Field::getName)
+                .toList();
     }
 }
