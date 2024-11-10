249f407966f7d1e961205d3c77565f9d9eea1feb
Your Name <you@example.com>
2024-11-10 19:39:26 +0200

    update files

--- src/main/java/comparus/service/util/StringUtil.java
+++ src/main/java/comparus/service/util/StringUtil.java
@@ -1,22 +1,37 @@
 package comparus.service.util;
 
 public class StringUtil {
-    public static int getFirstNumberByString(String value, String separator){
+    public static int getFirstNumberBySeparator(String value, String separator) {
         try {
             String first = value.substring(0, value.indexOf(separator));
             return Integer.parseInt(first);
-        } catch (Exception e){
-            // todo nothing
+        } catch (Exception e) {
+            return 0;
         }
-        return 0;
     }
-    public static int getSecondNumberByString(String value, String separator){
+
+    public static int getSecondNumberBySeparator(String value, String separator) {
         try {
-            String second = value.substring(value.indexOf(separator)+1);
+            String second = value.substring(value.indexOf(separator) + 1);
             return Integer.parseInt(second);
-        } catch (Exception e){
-            // todo nothing
+        } catch (Exception e) {
+            return 0;
         }
-        return 0;
+    }
+
+    public static String getFirstPartOfString(String value, String separator) {
+        try {
+            return value.substring(0, value.indexOf(separator));
+        } catch (Exception e) {
+            return value;
+        }
+    }
+
+    public static String getSecondPartOfString(String value, String separator) {
+        try {
+            return value.substring(value.indexOf(separator) + 1);
+        } catch (Exception e) {
+            return value;
+        }
     }
 }
