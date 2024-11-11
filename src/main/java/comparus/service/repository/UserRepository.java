feacbfe15689c12b9d4348a4eff8835e8f0381f6
Your Name <you@example.com>
2024-11-11 10:45:23 +0200

    UserRepository & UserService

---
+++ src/main/java/comparus/service/repository/UserRepository.java
@@ -0,0 +1,48 @@
+package comparus.service.repository;
+
+import comparus.service.configuration.DataSourceDetails;
+import comparus.service.domain.User;
+import comparus.service.util.StringUtil;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+@Slf4j
+public class UserRepository {
+
+    private List<User> getUsers(DataSourceDetails dataSourceDetails) {
+        String tableName = dataSourceDetails.getTable();
+        String sql = String.format("SELECT * FROM %s", tableName);
+
+        log.debug("getUsers: sql=" + sql);
+        JdbcTemplate jdbcTemplate = dataSourceDetails.getJdbcTemplate();
+
+        return jdbcTemplate.query(sql,
+                dataSourceDetails.getUserRowMapper());
+    }
+
+    public List<User> getUsersByFilter(DataSourceDetails dataSourceDetails, String filter) {
+        if (filter == null || filter.isEmpty()) {
+            return getUsers(dataSourceDetails);
+        }
+        String tableName = dataSourceDetails.getTable();
+
+        String filterFieldName = StringUtil.getFirstPartOfString(filter, "-");
+        filterFieldName = dataSourceDetails.getFieldNameForFilter(filterFieldName);
+        String filterValue = StringUtil.getSecondPartOfString(filter, "-");
+        log.debug("getUsersByFilter, tableName: {}, filterFieldName: {}, filterValue: {}", tableName, filterFieldName, filterValue);
+
+        String sql = String.format("SELECT * FROM %s WHERE %s LIKE ?", tableName, filterFieldName);
+        log.debug("getUsersByFilter: sql=" + sql);
+        JdbcTemplate jdbcTemplate = dataSourceDetails.getJdbcTemplate();
+
+        return jdbcTemplate.query(sql,
+                dataSourceDetails.getUserRowMapper(),
+                new Object[]{"%" + filterValue + "%"});
+    }
+}
--- src/main/java/comparus/service/service/UserService.java
+++ src/main/java/comparus/service/service/UserService.java
@@ -8,30 +8,23 @@ import comparus.service.util.UserComparator;
 import comparus.service.exception.InvalidParametersException;
 import comparus.service.repository.UserRepository;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 @Service
 @AllArgsConstructor
+@Slf4j
 public class UserService {
     private final UserRepository userRepository;
     private final ValidateRequestParameters validateRequestParameters;
     private final DataSourcesHolder dataSourcesHolder;
 
-    // this method present separate for UserServiceTest class
-    protected List<User> readUsersFromDataSourceHolderByFilter(String filter){
-        List<User> allUsers = new ArrayList<>();
-        for (DataSourceDetails dataSource : dataSourcesHolder.getDataSources().values()) {
-            allUsers.addAll(userRepository.getUsersByFilter(dataSource, filter));
-        }
-        return allUsers;
-    }
     public List<User> getUsersByFilters(String filter, String propagation, String order) {
         validateRequestParameters.checkFilterField(filter);
         validateRequestParameters.checkPropagationField(propagation);
@@ -42,9 +35,24 @@ public class UserService {
             allUsers.sort(new UserComparator(order));
         }
         if (propagation != null && !propagation.isEmpty()) {
-            int numberOfBacket = StringUtil.getFirstNumberByString(propagation, "-");
-            int countInBacket = StringUtil.getSecondNumberByString(propagation, "-");
+            int numberOfBacket = StringUtil.getFirstNumberBySeparator(propagation, "-");
+            int countInBacket = StringUtil.getSecondNumberBySeparator(propagation, "-");
             return allUsers.stream().skip((long) numberOfBacket * countInBacket).limit(countInBacket).toList();
+        }
+        return allUsers;
+    }
+    // this method present separate from getUsersByFilters for UserServiceTest class
+    protected List<User> readUsersFromDataSourceHolderByFilter(String filter){
+        List<User> allUsers = new ArrayList<>();
+        for (DataSourceDetails dataSource : dataSourcesHolder.getDataSources().values()) {
+            try {
+                log.debug("analyze: dataSource: " + dataSource.getName());
+                List<User> users = userRepository.getUsersByFilter(dataSource, filter);
+                log.debug("read from dataSource: users.size()=" + users.size());
+                allUsers.addAll(users);
+            } catch (Exception e) {
+                log.error("Invalid connection to "+dataSource+" with message: "+e.getMessage());
+            }
         }
         return allUsers;
     }
